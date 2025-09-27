e2a82086630ea0d2573ce0767b447bb662a1c9b6
mensahAmpomah <gideonmensah201@gmail.com>
2025-09-26 21:45:20 -0700

    Created the logout page

--- moviestore/accounts/views.py
+++ moviestore/accounts/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from .forms import CustomUserCreationForm, CustomErrorList
 from django.shortcuts import redirect
 
-from django.contrib.auth import login as auth_login, authenticate
+from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
+
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect('home')
 
 def login(request):
     template_data = {}
